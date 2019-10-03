dd05d8aa05a702feddf1f786b52959129b27c13b
Wesley <wesleypro37@gmail.com>
2019-10-03 08:41:09 -0300

    Atualização e correção de erros
    Corrigido falhas do temporizador. Acrescentado monitoramento via serial.

--- Controle_de_Nivel.ino
+++ Controle_de_Nivel.ino
@@ -1,18 +1,18 @@
 //*************************************************************************************************/
 //   Projeto    : Controle de Nivel
 //   Arquivo    : Controle_de_Nivel.ino
-//   Descrição  : Configurações e loop principal
+//   Descricao  : Configuracoes e loop principal
 //   Data       : 30/09/2019
 //*************************************************************************************************/
 
 //Parametros
-#define TIMER_SEGUNDOS 5*60  //5min
+#define TIMER_SEGUNDOS 300  //300 segundos = 5min
 
 //Entradas
 #define pinoSensorNivel1 2
 #define pinoSensorNivel2 3
 
-//Saídas
+//Saidas
 #define pinoAlarmeLed 4
 #define pinoAlarmeBux 5
 #define pinoBomba1    6
@@ -20,7 +20,8 @@
 
 void setup()
 {
-    // coloque aqui o seu código de configuração para ser executado uma vez:
+    // coloque aqui o seu codigo de configuracao para ser executado uma vez:
+    Serial.begin(9600);
     pinMode(pinoSensorNivel1, INPUT);
     pinMode(pinoSensorNivel2, INPUT);
     
@@ -32,18 +33,18 @@ void setup()
 
 void loop()
 {
-    // coloque seu código principal aqui, para executar repetidamente:
-    if(nivelBaixo() && !nivelCritico())                 //Se o nível esta baixo, mas não crítico
+    // coloque seu codigo principal aqui, para executar repetidamente:
+    if(nivelBaixo() && !nivelCritico())                 //Se o nivel esta baixo, mas nï¿½o crï¿½tico
     {
         ligaBomba();                                        //Liga bomba
         timer_seconds(0);                                  //Reinicia temporizador
-        while(!timer_seconds(TIMER_SEGUNDOS))              //Enquanto temporizador está rodando
+        while(!timer_seconds(TIMER_SEGUNDOS))              //Enquanto temporizador esta rodando
         {
-            verificaNivelCritico();                             //Verifica se o nível ficou crítico
+            verificaNivelCritico();                             //Verifica se o nivel ficou crï¿½tico
         }
         desligaBomba();                                     //Desliga a bomba ao final do temporizador
     }
-    verificaNivelCritico();                             //Sempre verifica o nível crítico ao final do ciclo
+    verificaNivelCritico();                             //Sempre verifica o nivel critico ao final do ciclo
 }
 
 bool nivelBaixo()
@@ -60,15 +61,18 @@ void ligaBomba()    //Liga bombas de forma alternada
 {
     static bool alternar = false;
     static bool blk = false;
-    if(nivelCritico() && !blk)                  //Se o nível estiver crítico e o bloqueio não estiver ativo
+    if(nivelCritico() && !blk)                  //Se o nivel estiver critico e o bloqueio nao estiver ativo
     {
         blk = true;                                 //Bloqueia a alternacia das bombas
     }
-    digitalWrite(pinoBomba1, !alternar);        //liga ou desliga bomba1 dependendo da variável de alternancia
-    digitalWrite(pinoBomba2, alternar);         //liga ou desliga bomba2 dependendo da variável de alternancia
-    if(!nivelCritico() && !blk)                 //Se o nível não estiver crítico
+    digitalWrite(pinoBomba1, !alternar);        //liga ou desliga bomba1 dependendo da variavel de alternancia
+    digitalWrite(pinoBomba2, alternar);         //liga ou desliga bomba2 dependendo da variavel de alternancia
+    Serial.print("Bomba " );
+    Serial.print(alternar + 1);
+    Serial.println(" acionada");
+    if(!nivelCritico() && !blk)                 //Se o nivel nao estiver critico
     {
-        alternar = !alternar;                       //Inverte a bomba que será ligada no próximo ciclo
+        alternar = !alternar;                       //Inverte a bomba que sera ligada no proximo ciclo
     }
 }
 
@@ -76,6 +80,7 @@ void desligaBomba()
 {
     digitalWrite(pinoBomba1, LOW);
     digitalWrite(pinoBomba2, LOW);
+    Serial.println("Bombas Desligadas");
 }
 
 void acionaAlarme()
@@ -87,29 +92,37 @@ void acionaAlarme()
 void verificaNivelCritico()
 {
     static bool alrm = false;
-    if(nivelCritico())                              //Se o nível estiver crítico
+    if(nivelCritico())                              //Se o nivel estiver critico
     {
         alrm = true;                                    //Habilita alarme
         ligaBomba();                                    //Liga a bomba
         timer_seconds(0);                              //Reinicia o timer
         while(!timer_seconds(TIMER_SEGUNDOS))          //Enquanto o timer estiver rodando
         {
-            acionaAlarme();                                 //Mantém o alamre acionado
+            acionaAlarme();                                 //Mantem o alamre acionado
         }
         desligaBomba();                                 //Desliga a bomba ao final do timer
     }
     if(alrm)                                         //Se o alarme estiver habilitado
     {
-        acionaAlarme();                                 //Mantém o alarme acionado
+        acionaAlarme();                                 //Mantem o alarme acionado
     }
 }
 
-bool timer_seconds(int tempo)       //Temporizador de segundos
+bool timer_seconds(unsigned long tempo)       //Temporizador de segundos
 {   
     static unsigned long timer = 0;
+    int timeMonitor = ((timer + tempo * 1000) - millis())/1000;
+    static int previousTime = 0;
     if(tempo == 0)
     {
         timer = millis();
+        return(false);
+    }
+    if(timeMonitor != previousTime)
+    {
+        Serial.println(timeMonitor);
+        previousTime = timeMonitor;
     }
     if(timer + tempo * 1000 < millis())
     {
@@ -118,12 +131,12 @@ bool timer_seconds(int tempo)       //Temporizador de segundos
     return(false);
 }
 
-bool pulse_1s()                     //Pulso contínuo de 1s
+bool pulse_1s()                     //Pulso continuo de 1s
 {
     static bool ret = true;
     static unsigned long timer = millis();
     if(timer + 500 < millis())
-    {
+    {   if(ret) Serial.println("ALARME");
         timer = millis();
         ret   = !ret;
     }
